2cf2b62e01848c1ebdfcbf43be17bfa0506648f9
Justin Sanders <justin.sanders@lakesideschool.org>
2018-02-01 08:42:46 -0800

    Screw you alex!

---
+++ chessEngine.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+
+class Board:
+    i = 100
+    position = 1
+    win = False
+    score = 0
+    
+    def __init__(self):
+        self.i = 10
+        self.position = np.empty([8,8])
+        for int i in ()
+    
+    def countScore(self):
+        score = 1
+        
+    def win():
+        win = true
+    
+    
+current = Board()
+print(current.i)
+print(current.position)
