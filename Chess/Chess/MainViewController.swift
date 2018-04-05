369a0acf2398430224f15dd0c02193d7d88509be
Eric <EricZ18@lakesideschool.org>
2018-04-05 08:10:52 -0700

    faq button

--- Chess/Chess/MainViewController.swift
+++ Chess/Chess/MainViewController.swift
@@ -15,6 +15,9 @@ class MainViewController: UIViewController {
     var portraitHeight: CGFloat = 0
     var portraitWidth: CGFloat = 0
     
+    // initialize intro label
+    let introLabel = UILabel(frame: CGRect(x:60, y:50,width: 300, height: 500))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.blue
@@ -55,6 +58,25 @@ class MainViewController: UIViewController {
         playButton.topAnchor.constraint(equalTo: (margins?.centerYAnchor)!).isActive = true
         playButton.heightAnchor.constraint(equalToConstant: portraitHeight/10)
         playButton.addTarget(self, action: #selector(MainViewController.playButtonPushed(_:)), for: UIControlEvents.touchUpInside)
+        
+        //initialize introduction stuff
+        introLabel.isHidden = true
+        introLabel.lineBreakMode = .byWordWrapping
+        introLabel.numberOfLines = 0
+        introLabel.backgroundColor = UIColor.white
+        
+        introLabel.text = "Move your pieces and take your opponents pieces to trap the opponent's King!\nPiece Moves: \nPawn : Can move forwards 1 space and takes pieces diagonally. Can move two spaces if being moved for the first time \n\nKnight : can move in a 3x2 or 2x3 L shape in any direction \n\nBishop : can move along diagonals \n\nRook : can move vertically and horizontally \n\nQueen : can move vertically, horizontally, and along diagonals \n\nKing can move one space in any direction \n\n Press How To Play again to close"
+        self.view.addSubview(introLabel)
+        
+        let button = UIButton()
+        button.frame = CGRect(x: 30, y: 500, width: 350, height: 80)
+        
+        button.backgroundColor = UIColor.black
+        button.setTitle("How To Play", for: UIControlState.normal)
+        button.titleLabel?.font = UIFont(name: "Times New Roman", size: portraitHeight/12)
+
+        button.addTarget(self, action: #selector(self.ButtonPressed(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
@@ -70,6 +92,18 @@ class MainViewController: UIViewController {
     
     @IBAction func unwindToMainViewController(segue: UIStoryboardSegue) {
         
+    }
+    
+    @IBAction func ButtonPressed(_ sender: UIButton!)
+    {
+        if(introLabel.isHidden == false)
+        {
+            introLabel.isHidden = true;
+        }
+        else
+        {
+            introLabel.isHidden = false;
+        }
     }
 }
 
